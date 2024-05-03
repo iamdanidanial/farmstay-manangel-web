30dfdcf8ddc3c04251d0bc1aa244d189baaf7a5d
iamdanidanial <iamdanidanial@gmail.com>
2024-05-03 15:42:29 +0700

    Penambahkan Review

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
 import farmstay from "../images/review1.svg";
 import photo from "../images/simonangie.jpg";
-import booking from "../images/bookingcom.png";
+import leonardo from "../images/guest/leonardo-switzerland.jpg"
+import ava_j from "../images/guest/ava-j.png"
+import ava_a from "../images/guest/ava-a.png"
+import ava_h from "../images/guest/ava-h.png"
+import ava_l from "../images/guest/ava-l.png"
+import hans from "../images/guest/hans.webp"
+import lucie from "../images/guest/lucie.webp"
+import melissa from "../images/guest/melissa.webp"
+import nico from "../images/guest/nico.webp"
+import sara from "../images/guest/sara.webp"
+import yakub from "../images/guest/yakub.webp"
+import karla from "../images/guest/karla.webp"
+
+
 
 export function PlatformBadge({ item }) {
   let className;
@@ -103,79 +116,150 @@ export const Review = () => {
       title: "Fantastis!",
       review:
         "Ari, Uyung and Uyungs mom make you feel immediatly at home. We would say: if you are looking for authentic adventure, peace and quiet, learning about the culture of Indonesia and about farming, a good laugh, a warm family feeling: GO TO FARMSTAY. Beautiful and clean place to stay!",
-      image: photo,
+      image: ava_h,
       country: "Belgia",
       platform: "Google Maps",
     },
     {
       id: 2,
-      name: "Lieve",
-      title: "Beautifull, calming & heartwarming.",
+      name: "Leandro",
+      title: "Wonderful stay for all lovers of nature and good society, I absolutely recommend it!",
       review:
-        "Ari and Uyung made sure our stay was as pleasent as possible and showed us their home and their homevillage. The surroundings are very beautifull. Totally worth it!",
-      image: photo,
-      country: "Belanda",
+        "Wonderful stay for all lovers of nature, I absolutely recommend it! The guys of the staff built everything on their own, are super kind and it feels like being part of a family. They showed me the surrounding area with wonderful terraced paddy fields and banana plantations.",
+      image: leonardo,
+      country: "Switzerland",
       platform: "Booking.com",
     },
     {
       id: 3,
-      name: "Anna",
-      title: "",
-      review: "lorem lorem lorem",
-      image: photo,
-      country: "Italia",
+      name: "Lieve",
+      title: "Beautifull, calming & heartwarming.",
+      review:
+        "Ari and Uyung made sure our stay was as pleasent as possible and showed us their home and their homevillage. The surroundings are very beautifull. Totally worth it!",
+      image: ava_l,
+      country: "Belanda",
       platform: "Booking.com",
     },
     {
       id: 4,
-      name: "Dani dddd",
-      title: "",
-      review:
-        "ri and Uyung made sure our stay was as pleasent as possible and showed us their home and their homevillage. The surroundings are very beautifull. Totally worth it!",
-      image: photo,
+      name: "Anna",
+      title: "Magico 💫",
+      review: "Ospitalità e gentilezza assoluta in questa splendida famiglia nel cuore della natura. Raccomandatissimo per chi vuole vivere esperienze autentiche nella realtà locale",
+      image: ava_a,
       country: "Italia",
-      platform: "Airbnb.com",
+      platform: "Booking.com",
     },
     {
       id: 5,
-      name: "Luar biasa",
-      title: "",
+      name: "Jassim",
+      title: "Exceptional",
       review:
-        "Mas Ari dan Mas Uyung sangat membantu mulai dari awal mencari titik penginapan hingga tiba saatnya pulang. Serasa main ke rumah nenek di desa. Tersedia banyak aktivitas yang disediakan",
-      image: photo,
-      country: "Indonesia",
-      platform: "airbnb.com",
-    },
+        "t's the right places for people who want to explore a real local cultural life, we just stayed two nights but we felt like we're staying with our family.",
+      image: ava_j,
+      country: "Bahrain",
+      platform: "Booking.com",
+    }, 
     {
       id: 6,
       name: "Abbey",
       title: "Incredible place and hosts",
       review:
         "This stay was so cozy, overlooking the hills, and so peaceful. We loved it",
-      image: photo,
+      image: ava_a,
       country: "Italia",
       platform: "Booking.com",
     },
     {
       id: 7,
-      name: "Abbey",
-      title: "Incredible place and hosts",
+      name: "Abdulrhman",
+      title: "Best experience in Indonesia so far",
       review:
-        "This stay was so cozy, overlooking the hills, and so peaceful. We loved it",
-      image: photo,
-      country: "Italia",
+        "- Staff were helpful with everything, Ari and Uyung are the best, Uyung mom she is the one preparing food and doing the cleaning. the way they treated me in there, I felt like it’s my own home, they did take me around the village, we also did some fun activities together, I won’t forget the good times I spent in the farmstay, thank you guys so much, - the views, it’s breathtaking, - the simple and quiet life in there that I enjoyed a lot , escaping from reality and from big noisy life in city’s 😅, - the friendly villagers",
+      image: ava_a,
+      country: "Saudi Arabia",
       platform: "Booking.com",
     },
     {
       id: 8,
-      name: "Abbey",
-      title: "Incredible place and hosts",
+      name: "Julia",
+      title: "Exceptional",
       review:
-        "This stay was so cozy, overlooking the hills, and so peaceful. We loved it",
-      image: photo,
-      country: "Italia",
+        "Ari and Uyung are amazing hosts and make you feel at home from the very first moment. Mananggel the right place for anybody who wants to experience the rural life of Indonesia including its food culture. Big recommendations for this farmstay.",
+      image: ava_j,
+      country: "Austria",
       platform: "Booking.com",
     },
+    {
+      id: 10,
+      name: "Hans",
+      title: "The accommodation was truly a back to nature experience",
+      review:
+        "We were in the jungle. The people of the homestay used everything from nature for both the food and the accommodation. The food was delicious, little meat, but fine replacements for that. It was super fairy with Ari, Uyung and Harris. A lot of talking to them, 2 hikes done, one with homemade lunch during the hike. This farmstay is highly recommended to nature lovers. Ari, Uyung, Harris (July), thanks for the great days, we will remember it often. who knows, see you soon.",
+      image: hans,
+      country: "Netherlands",
+      platform: "Airbnb.com",
+    },
+    {
+      id: 11,
+      name: "Lucía",
+      title: "I can't say enough about Ari and his friends hospitality!",
+      review:
+        "They made us feel at home and he was extremely attentive and helpful! The area is gorgeous and the food was amazing. I think we had to experience a real Indonesian experience staying at his place. As is a little bit secluded make sure you stay at least 3 nights or more so you get to enjoy more of what the area has to offer. Thank you Ari for everything, you are a great host!",
+      image: lucie,
+      country: "Indiana",
+      platform: "Airbnb.com",
+    },
+    {
+      id: 12,
+      name: "Mélissa",
+      title: "Endroit incroyable avec des hôtes très accueillants, bienveillants et ouverts",
+      review:
+        "J’ai passé de très beaux moments, de partage, de découverte, mais aussi de rire. Vous êtes entourés de nature et d’adorables animaux qui vous donneront plein d’amour. Sand parler de la nourriture proposée qui est excellente. Je recommande à 100% sans hésitation.",
+      image: melissa,
+      country: "France",
+      platform: "Airbnb.com",
+    },
+    {
+      id: 13,
+      name: "Nico",
+      title: "We had two incredibly beautiful days",
+      review:
+        "We had two incredibly beautiful days at Ari's on his farm. With other guests, we went on excursions together and ate together with everyone in the evening. The accommodation is fantastic and in the middle of the nature. We thank you for the great experience :).",
+      image: nico,
+      country: "Germany",
+      platform: "Airbnb.com",
+    },
+    {
+      id: 14,
+      name: "Sára",
+      title: "the farm was fantastic!",
+      review:
+        "Our stay at the farm was fantastic! The place is beautiful and right in the nature. You just can't get enough of that view from the window! Ari and the guys are super kind and always helpful. Ari is also an amazing tour gide, he showed us the best places around the town.",
+      image: sara,
+      country: "Hungary",
+      platform: "Airbnb.com",
+    },
+    {
+      id: 15,
+      name: "Karla",
+      title: "Great to stay",
+      review:
+        "I had a great time at the Airbnb. It is surrounded by a lot of nature and a great landscape. The hosts are very helpful and showed me typical indonesian food in addition to the area. I highly recommend an overnight stay",
+      image: karla,
+      country: "Germany",
+      platform: "Airbnb.com",
+    },
+    {
+      id: 16,
+      name: "Jákup Á Rógvi J",
+      title: "100% Recomended",
+      review:
+        "This is a really nice place if you want to get know about the local Indonesian culture, the environment here is so beautiful. Ari is the best host ever, and his friends are really friendly too. I would 100% recommend",
+      image: yakub,
+      country: "Farue Island",
+      platform: "Airbnb.com",
+    },
+   
   ];
   return (
     <>
@@ -217,7 +301,7 @@ export const Review = () => {
             </div>
 
             <div className="w-full">
-              <div className="grid grid-cols-2 gap-2 ">
+              <div className="grid md:grid-cols-1 grid-cols-2 gap-2 ">
                 {dataReview.slice(0, 4).map((item) => (
                   <div
                     key={item.id}
@@ -263,7 +347,7 @@ export const Review = () => {
               <h1 className="md:text-2xl text-5xl font-bold text-headingcolor mb-5 text-blue-700">
                 Booking.com
               </h1>
-              <div className="grid grid-cols-4 gap-2">
+              <div className="grid grid-cols-4 md:grid-cols-1 gap-2">
                 {dataReview.slice(0, 7).map((review) => (
                   <ReviewCard key={review.id} review={review} />
                 ))}
@@ -275,7 +359,7 @@ export const Review = () => {
                 >
                   <span className="absolute inset-0 z-10 bg-blue-100"></span>
                   <span className="z-20 relative text-gray-800 text-lg">
-                    See All Review From{" "}
+                    See all review from{" "}
                     <p className="text-blue-500 text-2xl">BOOKING.COM</p>
                   </span>
                 </a>
@@ -286,28 +370,30 @@ export const Review = () => {
             <h1 className="md:text-2xl text-5xl font-bold text-headingcolor mb-5 text-red-500">
               Airbnb.com
             </h1>
-            <div className="grid grid-cols-4 gap-2">
+            <div className="grid grid-cols-4 md:grid-cols-1 gap-2">
               {dataReview
                 .filter((review) => review.platform === "Airbnb.com")
                 .map((review) => (
                   <ReviewCard key={review.id} review={review} />
                 ))}
-              <div className="border border-grey-400 text-center flex justify-center font-semibold items-center bg-cover bg-center relative">
-                <span className="absolute inset-0 z-10 bg-black opacity-50"></span>
-                <span className="z-20 relative text-white">SEE ALL REVIEW</span>
-                <img
-                  src={farmstay}
-                  alt="Background Image"
-                  className="absolute inset-0 object-cover z-0 h-full w-full"
-                />
-              </div>
+              <a
+                  href="https://www.airbnb.com/rooms/747463323767904056/reviews?adults=1&category_tag=Tag%3A8678&children=0&infants=0&check_in=2024-05-15&check_out=2024-05-20&source_impression_id=p3_1714720822_8NJOEtbG2hOozpcA&previous_page_section_name=1000&federated_search_id=4d8c5c53-e159-4d54-9a1e-9c70e3f586a1"
+                  target="_blank"
+                  className="cursor-pointer border border-grey-400 text-center flex justify-center font-semibold items-center bg-cover bg-center relative rounded-md"
+                >
+                  <span className="absolute inset-0 z-10 bg-red-100"></span>
+                  <span className="z-20 relative text-gray-800 text-lg">
+                    See all review from{" "}
+                    <p className="text-blue-500 text-2xl">AIRBNB.COM</p>
+                  </span>
+                </a>
             </div>
           </div>
           <div className="flex flex-col w-full mt-5">
             <h1 className="md:text-2xl text-5xl font-bold text-headingcolor mb-5 text-yellow-500">
               Google Maps
             </h1>
-            <div className="grid grid-cols-4 gap-2">
+            <div className="grid grid-cols-4 md:grid-cols-1 gap-2">
               {dataReview
                 .filter((review) => review.platform === "Google Maps")
                 .map((review) => (
